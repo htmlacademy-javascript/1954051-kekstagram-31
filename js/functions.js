958ccd607516fb5323c6e4a1f3d401ae1eeb879c
Alexander Poletaev <poletaev.alex@gmail.com>
2024-02-24 12:01:34 +0300

    добавляет файл с функциями и тремя заданиями в нём

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+
+//Функция для проверки длины строки
+
+const checkStringLength = (string, maxStringLength) => {
+return string.length <= maxStringLength
+};
+
+// строка короче 20 символов
+checkStringLength ('Одри умная кисонька', 20);
+// строка длиннее 20 символов
+checkStringLength ('Одри умная кисонька и она любит собак', 20);
+
+
+
+// Функция для проверки, является ли строка палиндромом
+
+const isPalindrom = (string) => {
+  let normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let stringReversed = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    stringReversed += normalizedString[i];
+  }
+  return normalizedString === stringReversed;
+};
+//строка является палиндромом
+console.log(isPalindrom('И у облаков вокал Боуи'));
+// и это палиндром
+console.log(isPalindrom('топот'));
+//а это нет
+console.log(isPalindrom('палиндром'));
+
+
+
+// Функция принимает строку и возвращает число
+
+ const returnsDigits = (string) => {
+  const normalizedString = string.replaceAll(' ', '');
+  let result = ''
+for (let i = 0; i < normalizedString.length; i++) {
+if (!Number.isNaN(parseInt(normalizedString[i], 10))) {
+result += normalizedString[i];
+}
+}
+return result ? +result : NaN;
+};
+returnsDigits('2023 год 12 месяц')
+console.log(returnsDigits('2023 год 12 месяц'));
