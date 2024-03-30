ab6c5d2bf974660ae22c8877c531be8236951682
Alexander Poletaev <poletaev.alex@gmail.com>
2024-03-30 10:50:35 +0300

    исправляет ошибки

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,30 +1,42 @@
-
-// На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
-
-// Адрес изображения url подставьте как атрибут src изображения.
-// Описание изображения description подставьте в атрибут alt изображения.
-// Количество лайков likes выведите в блок .picture__likes.
-// Количество комментариев comments выведите в блок .picture__comments.
-// Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
-
-// import {createPhotoDescriptions} from './data.js';
-
+import { openUserModal } from './modal.js';
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture'); //шаблон
 
 const picturesArea = document.querySelector('.pictures'); // место, куда записываем сгенерированные фото
 
-const photoDescriptions = (photo) => {
+const localPhotos = [];
+
+const clearLocalPhotos = () => {
+  localPhotos.length = 0;
+};
+
+const photoDescriptions = (photos) => {
+  clearLocalPhotos();
+  localPhotos.push(...photos.slice());
   const picturesFragment = document.createDocumentFragment();
-  photo.forEach(({url, description, likes, comments}) => {
+  photos.forEach(({ id, url, description, likes, comments }) => {
     const pictureClone = pictureTemplate.cloneNode(true);
+    pictureClone.dataset.pictureId = id; // добавляет id миниатюрам
     pictureClone.querySelector('.picture__img').src = url;
     pictureClone.querySelector('.picture__img').alt = description;
     pictureClone.querySelector('.picture__likes').textContent = likes;
     pictureClone.querySelector('.picture__comments').textContent = comments.length;
     picturesFragment.appendChild(pictureClone);
-    picturesArea.appendChild(pictureClone);
   });
+  picturesArea.appendChild(picturesFragment);
 };
-export { photoDescriptions };
 
 
+picturesArea.addEventListener('click', (evt) => {
+  const currentPictureElement = evt.target.closest('.picture');
+  if (currentPictureElement) {
+    evt.preventDefault(); // предотвращаем открытие в новом окне
+    // тут мы должны сопоставить в переменной id фото c id, добавленным через dataset
+    // const currentPhoto =
+    const currentId = Number(currentPictureElement.dataset.pictureId);
+    const currentPhotoData = localPhotos.find((item) => item.id === currentId);
+    openUserModal(currentPhotoData);
+  }
+});
+
+
+export { photoDescriptions};
